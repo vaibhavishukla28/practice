49a5d4861bd1fddd91d830a3f8e91e8213e37a1f
Vaibhavi Shukla <vaibhavishukla29@gmail.com>
2023-05-31 10:20:07 +0530

    Create two_digit_sum.py

---
+++ two_digit_sum.py
@@ -0,0 +1,7 @@
+no=input("enter two digit no")
+first_digit=no[0]
+print("first digit:",first_digit)
+second_digit=no[1]
+print("second digit:",second_digit)
+result=int(first_digit)+int(second_digit);
+print(result)
